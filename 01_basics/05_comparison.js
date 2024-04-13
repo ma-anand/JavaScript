99a3a6ff3a5f03e575d75b5d53260f1f3f1f26c2
ma-anand <aananad20-comp@bvucoep.edu.in>
2024-04-14 01:02:55 +0530

    Comparison of Data Type

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,39 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1); 
+
+
+
+
+// console.log("2" > 1);       //true
+// console.log("02" > 1);      //true
+
+
+console.log("2" === 2);        //false
+console.log("2" === "2");      //true
+
+console.log("2" !== 3);        //true
+console.log("2" !== "3");      //true
+console.log("2" !== "2");      //false
+
+
+
+
+//NULL
+
+// console.log(null > 0);        //false
+// console.log(null < 0);        //false
+// console.log(null == 0);       //false
+// console.log(null >= 0);       //true      beacuse it treate null as 0.
+
+
+//Undefined
+
+// console.log(undefined > 0);        //false
+// console.log(undefined < 0);        //false
+// console.log(undefined == 0);       //false
+// console.log(undefined >= 0);       //false
+
