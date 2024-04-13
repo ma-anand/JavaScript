fcaafb371ea6f910ae76d8c3d1034a3530af48cf
ma-anand <aananad20-comp@bvucoep.edu.in>
2024-04-14 02:28:43 +0530

    DataTypes-Summary

--- 01_basics/DataTypes-Summary.js
+++ 01_basics/DataTypes-Summary.js
@@ -2,15 +2,15 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100;
+const scoreValue = 100.3;
 
-const isLoggedIn = false
-const outsideTemp = null
+const isLoggedIn = false;
+const outsideTemp = null;
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol('123');
+const anotherId = Symbol('123');
 
 console.log(id === anotherId);
 
@@ -34,4 +34,4 @@ const myFunction = function(){
 
 console.log(typeof anotherId);
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+// https://262.ecma-international.org/5.1/#sec-11.4.3   
