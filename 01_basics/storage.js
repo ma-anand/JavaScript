f284b169dd50d3791cd4b3bd65e70e45b40bedaf
ma-anand <aananad20-comp@bvucoep.edu.in>
2024-04-14 02:59:49 +0530

    storage

--- 01_basics/storage.js
+++ 01_basics/storage.js
@@ -1,29 +1,29 @@
-//   Stack (Primitive)
+//   Stack (Primitive)        it get copy of the value
 
-// let name1="Anurag";
-// let name2 = name1;
+let name1="Anurag";
+let name2 = name1;
 
 // console.log(name2);         //Anurag
 
-// name2="Anand";
+name2="Anand";
 
 // console.log(name1);         //Anurag
 // console.log(name2);         //Anand
 
 
-//   Heap (Non-Primitive)
+//   Heap (Non-Primitive)      it get reference only
 
-// let userOne = {
-//     Name: "Anurag",
-//     PRN: 1
-// }
+let userOne = {
+    Name: "Anurag",
+    PRN: 1
+}
 
 // let userTwo = userOne;
 
 // console.log(userTwo);          //{ Name: 'Anurag', PRN: 1 }
 
-// userTwo.Name= "Anand";
-// userTwo.PRN= 2;
+userTwo.Name= "Anand";
+userTwo.PRN= 2;
 
 // console.log(userOne);          //{ Name: 'Anand', PRN: 2 }
 // console.log(userTwo);          //{ Name: 'Anand', PRN: 2 }
