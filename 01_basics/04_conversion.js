97f3dc8fcb63dc78f3f63766f51d7664fe6f72f2
ma-anand <aananad20-comp@bvucoep.edu.in>
2024-04-14 00:31:02 +0530

    conversion

--- 01_basics/04_conversion.js
+++ 01_basics/04_conversion.js
@@ -98,10 +98,10 @@ let str2 = " Anurag"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);                //"12"
+// console.log(1 + "2");                //"12"
+// console.log("1" + 2 + 2);            //"122"
+// console.log(1 + 2 + "2");            //"32"
 
 // console.log( (3 + 4) * 5 % 3);
 
@@ -110,11 +110,15 @@ let str3 = str1 + str2
 
 let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2;
+//console.log(num1);               //4
+//console.log(num2);               //4
+//console.log(num3);               //4
+
 
 let gameCounter = 100
 ++gameCounter;
-console.log(gameCounter);
+//console.log(gameCounter);             //101
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
