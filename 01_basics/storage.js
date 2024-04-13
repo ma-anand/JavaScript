2c1d8f7be9721a62989c2fb970eea70f705b634a
ma-anand <aananad20-comp@bvucoep.edu.in>
2024-04-14 02:55:18 +0530

    Storage Type

---
+++ 01_basics/storage.js
@@ -0,0 +1,29 @@
+//   Stack (Primitive)
+
+// let name1="Anurag";
+// let name2 = name1;
+
+// console.log(name2);         //Anurag
+
+// name2="Anand";
+
+// console.log(name1);         //Anurag
+// console.log(name2);         //Anand
+
+
+//   Heap (Non-Primitive)
+
+// let userOne = {
+//     Name: "Anurag",
+//     PRN: 1
+// }
+
+// let userTwo = userOne;
+
+// console.log(userTwo);          //{ Name: 'Anurag', PRN: 1 }
+
+// userTwo.Name= "Anand";
+// userTwo.PRN= 2;
+
+// console.log(userOne);          //{ Name: 'Anand', PRN: 2 }
+// console.log(userTwo);          //{ Name: 'Anand', PRN: 2 }
